26c08da5e6dbd36d1506263d6002b51a846995af
VisaScout <visascoutllc@gmail.com>
2024-10-08 11:18:29 +0530

    port has been changed

--- api/index.js
+++ api/index.js
@@ -36,6 +36,6 @@ cron.schedule('*/30 * * * *', () => {
     checkWebsiteStatus();
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000!!');
+app.listen(3001, () => {
+    console.log('Server is running on port 3001');
 });
